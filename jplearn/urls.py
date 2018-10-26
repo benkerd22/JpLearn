0fda4c602d9d1d8fa307b458d361f40755a05035
benker <benkerd22@gmail.com>
2018-10-26 20:28:07 +0800

    修改刷新方式为AJAX；优化了UI界面

--- jplearn/urls.py
+++ jplearn/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('audio/<str:q>/', views.audio, name='audio'),
+    path('test/', views.test, name='test'),
+    path('test/next', views.next, name='next'),
+    path('test/ttf', views.ttf, name='ttf'),
+    path('test/audio/<str:q>/', views.audio, name='audio'),
 ]
--- jplearn/views.py
+++ jplearn/views.py
@@ -1,11 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, FileResponse, JsonResponse
 import random
 import urllib
 
 import os
 
-def index(request):
+with open('jplearn/dictionary/dic.txt', 'r', encoding='UTF-8') as f:
+    dic = [line.split() for line in f] # dic include 漢字、仮名
+
+with open('jplearn/dictionary/chn.txt', 'r', encoding='UTF-8') as f:
+    chn = [line.strip() for line in f] # chn is 中文 only
+
+with open('jplearn/dictionary/tone.txt', 'r', encoding='UTF-8') as f:
+    tone = [line.strip() for line in f]
+
+hiragana = ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ひ', 'ふ', 'へ', 'ほ', 'ま', 'み', 'む', 'め', 'も', 'や', '', 'ゆ', '', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'わ', '', '', '', 'を', 'ん']
+katakana = ['ア', 'イ', 'ウ', 'エ', 'オ', 'カ', 'キ', 'ク', 'ケ', 'コ', 'サ', 'シ', 'ス', 'セ', 'ソ', 'タ', 'チ', 'ツ', 'テ', 'ト', 'ナ', 'ニ', 'ヌ', 'ネ', 'ノ', 'ハ', 'ヒ', 'フ', 'ヘ', 'ホ', 'マ', 'ミ', 'ム', 'メ', 'モ', 'ヤ', '', 'ユ', '', 'ヨ', 'ラ', 'リ', 'ル', 'レ', 'ロ', 'ワ', '', '', '', 'ヲ', 'ン']
+roman = ['a', 'i', 'u', 'e', 'o']
+mode = ['kanji', 'gana', 'chn', 'play'] # which HTML element should be visible
+tonestr = '⓪①②③④⑤⑥⑦⑧⑨⑩'
+
+def audio(request, q):
+    fname = urllib.parse.unquote(q)
+    fname = 'jplearn/audio/' + fname + '.mp3'
+    if os.path.exists(fname):
+        return FileResponse(open(fname, 'rb'))
+    raise Http404()
+
+def ttf(request):
+    return FileResponse(open('E:\\1.ttf', 'rb'))
+
+def test(request):
+    return render(request, 'jplearn/index.html', getNextContext())
+
+def getNextContext():
     choice = random.randint(0, len(dic) - 1)
 
     context = {'kanji':dic[choice][-1], 'gana':dic[choice][0], 'chn':chn[choice]}
@@ -34,28 +62,10 @@ def index(request):
         
         context['urls'].append({'name':url, 'alt':x})
 
-    return render(request, 'jplearn/index.html', context)
+    return context
 
-def audio(request, q):
-    fname = urllib.parse.unquote(q)
-    try:
-        with open('jplearn/audio/' + fname + '.mp3', 'rb') as f:
-            mp3 = f.read()
-        return HttpResponse(mp3)
-    except:
-        raise Http404()
+def next(request):
+    return JsonResponse(getNextContext())
 
-with open('jplearn/dictionary/dic.txt', 'r', encoding='UTF-8') as f:
-    dic = [line.split() for line in f] # dic include 漢字、仮名
 
-with open('jplearn/dictionary/chn.txt', 'r', encoding='UTF-8') as f:
-    chn = [line.strip() for line in f] # chn is 中文 only
 
-with open('jplearn/dictionary/tone.txt', 'r', encoding='UTF-8') as f:
-    tone = [line.strip() for line in f]
-
-hiragana = ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ひ', 'ふ', 'へ', 'ほ', 'ま', 'み', 'む', 'め', 'も', 'や', '', 'ゆ', '', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'わ', '', '', '', 'を', 'ん']
-katakana = ['ア', 'イ', 'ウ', 'エ', 'オ', 'カ', 'キ', 'ク', 'ケ', 'コ', 'サ', 'シ', 'ス', 'セ', 'ソ', 'タ', 'チ', 'ツ', 'テ', 'ト', 'ナ', 'ニ', 'ヌ', 'ネ', 'ノ', 'ハ', 'ヒ', 'フ', 'ヘ', 'ホ', 'マ', 'ミ', 'ム', 'メ', 'モ', 'ヤ', '', 'ユ', '', 'ヨ', 'ラ', 'リ', 'ル', 'レ', 'ロ', 'ワ', '', '', '', 'ヲ', 'ン']
-roman = ['a', 'i', 'u', 'e', 'o']
-mode = ['kanji', 'gana', 'chn', 'play'] # which HTML element should be visible
-tonestr = '⓪①②③④⑤⑥⑦⑧⑨⑩'
