c8dc70b66d3d34a9439e5eff9b4dd9e9afc6028e
benker <benkerd22@gmail.com>
2018-10-29 00:05:02 +0800

    优化小测模式

--- jplearn/urls.py
+++ jplearn/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='jplearn/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='jplearn/logout.html'), name='logout'),
     path('test/', views.test, name='test'),
-    path('test/next', views.next, name='next'),
+    path('start/', views.start, name='start'),
     path('cdn/ttf', views.ttf, name='ttf'),
     path('cdn/audio', views.audio, name='audio'),
     path('cdn/audio/<str:q>/', views.audio),
--- jplearn/views.py
+++ jplearn/views.py
@@ -1,23 +1,46 @@
+from functools import wraps
 from django.shortcuts import render
-from django.http import HttpResponse, Http404, FileResponse, JsonResponse
+from django.http import *
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from jplearn.models import Word, Realuser
 import random
 import urllib
+import json
+
 
 import os
-    
 
+'''
 hiragana = ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と', 'な', 'に', 'ぬ', 'ね',
             'の', 'は', 'ひ', 'ふ', 'へ', 'ほ', 'ま', 'み', 'む', 'め', 'も', 'や', '', 'ゆ', '', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'わ', '', '', '', 'を', 'ん']
 katakana = ['ア', 'イ', 'ウ', 'エ', 'オ', 'カ', 'キ', 'ク', 'ケ', 'コ', 'サ', 'シ', 'ス', 'セ', 'ソ', 'タ', 'チ', 'ツ', 'テ', 'ト', 'ナ', 'ニ', 'ヌ', 'ネ',
             'ノ', 'ハ', 'ヒ', 'フ', 'ヘ', 'ホ', 'マ', 'ミ', 'ム', 'メ', 'モ', 'ヤ', '', 'ユ', '', 'ヨ', 'ラ', 'リ', 'ル', 'レ', 'ロ', 'ワ', '', '', '', 'ヲ', 'ン']
-roman = ['a', 'i', 'u', 'e', 'o']
-mode = ['kanji', 'gana', 'chn', 'play']  # which HTML element should be visible
+roman = ['a', 'i', 'u', 'e', 'o']'''
 
 
+def login_(function=None, status=302):
 
-@login_required(login_url='login')
+    def decorator(view_func):
+        @wraps(view_func)
+        def _wrapped_view(request, *args, **kwargs):
+            if request.user.is_authenticated:
+                return view_func(request, *args, **kwargs)
+
+            if status == 302:
+                return HttpResponseRedirect(str(reverse_lazy('jplearn:login')) + '?next=' + str(reverse_lazy('jplearn:index')))
+            elif status == 403:
+                return HttpResponseForbidden()
+            else:
+                return Http404()
+        return _wrapped_view
+
+    if function:
+        return decorator(function)
+    return decorator
+
+
+@login_(status=403)
 def audio(request, q):
     fname = urllib.parse.unquote(q)
     fname = 'jplearn/audio/' + fname + '.mp3'
@@ -26,14 +49,87 @@ def audio(request, q):
     raise Http404()
 
 
-@login_required(login_url='login')
+@login_(status=403)
 def ttf(request):
     return FileResponse(open('jplearn/font/my.ttf', 'rb'))
 
 
-@login_required(login_url='login')
+def getnext(request):
+    user = request.user.realuser
+
+    mode = request.session['mode']
+    arrangment = request.session.get('arrangment', [])
+    current = request.session.get('current', -1)
+    current += 1
+
+    if mode == 'random':
+        selected_list = request.session['range']
+        if 0 in selected_list and not 1 in selected_list:
+            real_list = user.liked_words.all()
+        else:
+            real_list = Word.objects.all()
+
+        word = random.choice(real_list)
+    elif mode == 'round':
+        if current == len(arrangment):
+            request.session['in_test'] = False
+            return JsonResponse({
+                'status': 'finish',
+                'max': len(arrangment),
+            })
+
+        wordpk = arrangment[current]
+        word = Word.objects.get(pk=wordpk)
+
+        request.session['current'] = current
+    else:
+        return HttpResponseBadRequest()
+
+    res = {
+        'status': 'continue',
+        'current': current,
+        'max': len(arrangment),
+        'kanji': word.kanji,
+        'gana': word.gana,
+        'tone': word.tone,
+        'chn': word.chn,
+        'id': word.pk,
+    }
+
+    return JsonResponse(res)
+
+
+@login_
 def test(request):
-    return render(request, 'jplearn/index.html')
+    if True:
+        in_test = request.session.get('in_test', False)
+
+        if request.method == 'GET':
+            if in_test:
+                return render(request, 'jplearn/index.html')
+
+            return HttpResponseRedirect(reverse_lazy('jplearn:start'))
+
+        if request.method == 'POST':
+            if in_test:
+                data = json.loads(request.body)
+                action = data['action']
+
+                if action == 'getnext':
+                    return getnext(request)
+
+                if action == 'quit':
+                    request.session['in_test'] = False
+                    return JsonResponse({
+                        'status': 302,
+                        'location': reverse_lazy('jplearn:test'),
+                    })
+
+            return HttpResponseBadRequest()
+
+        return HttpResponseNotAllowed(['GET', 'POST'])
+    else:
+        return HttpResponseBadRequest()
 
 
 '''
@@ -68,37 +164,87 @@ def test(request):
     return context'''
 
 
-@login_required(login_url='login')
-def next(request):
+def initTest(request):
     user = request.user.realuser
-    liked_words = user.liked_words.all()
-    
-    if liked_words.count() == 0:
-        return JsonResponse({})
 
-    word = random.choice(liked_words)
+    data = json.loads(request.body)
+    selected_list = data['selected_list']
+    mode = data['mode']
 
-    res = {
-        'kanji': word.kanji,
-        'gana': word.gana,
-        'tone': word.tone,
-        'chn': word.chn,
-        'id': word.pk,
-    }
+    all_words = Word.objects.all()
+    my_words = user.liked_words.all()
+    # Todo: create a WordList Model
 
-    return JsonResponse(res)
+    if 0 in selected_list and not 1 in selected_list:
+        real_list = my_words
+    else:
+        real_list = all_words
+
+    request.session['in_test'] = True
+    if mode == 'round':
+        request.session['mode'] = 'round'
+        arrangment = [word.pk for word in real_list]
+        random.shuffle(arrangment)
+        request.session['arrangment'] = arrangment
+        request.session['current'] = -1
+    else:
+        request.session['mode'] = 'random'
+        request.session['range'] = selected_list
+
+    return JsonResponse({
+        'status': 302,
+        'location': reverse_lazy('jplearn:test'),
+    })
+
+
+@login_
+def start(request):
+    user = request.user.realuser
+    in_test = request.session.get('in_test', False)
+
+    if request.method == 'GET':
+        if not in_test:
+            context = {
+                "exists_list": [
+                    {
+                        'name': 'My Book',
+                        'count': user.liked_words.all().count(),
+                        'valid': user.liked_words.all().count() >= 0,
+                    },
+                    {
+                        'name': '《综合日语》（一）',
+                        'count': Word.objects.all().count(),
+                        'valid': True,
+                    }
+                ],
+            }
+
+            return render(request, 'jplearn/start.html', context)
+
+        return HttpResponseRedirect(reverse_lazy('jplearn:test'))
+
+    if request.method == 'POST':
+        if in_test:
+            return HttpResponseBadRequest()
+
+        # try:
+        return initTest(request)
+        # except:
+        return HttpResponseBadRequest()
+
+    return HttpResponseNotAllowed(['GET', 'POST'])
 
 
 def welcome(request):
     return render(request, "jplearn/welcome.html")
 
 
-@login_required(login_url='login')
+@login_(status=403)
 def dict(request):
     return render(request, 'jplearn/dictionary.html')
 
 
-@login_required(login_url='login')
+@login_(status=403)
 def dictData(request):
     user = request.user.realuser
 
@@ -118,20 +264,28 @@ def dictData(request):
     return JsonResponse(res)
 
 
-@login_required(login_url='login')
+@login_(status=403)
 def dictAction(request):
+    if request.method != 'POST':
+        return HttpResponseNotAllowed(['POST'])
+
     user = request.user.realuser
-    method = request.GET.get('m', None)
-    word_id = request.GET.get('q', None)
 
-    word = Word.objects.get(pk=word_id)
-    if method == 'add':
-        user.liked_words.add(word)
-        status = 1
-    elif method == 'remove':
-        user.liked_words.remove(word)
-        status = 0
-    else:
-        raise Http404()
+    try:
+        data = json.loads(request.body)
+        action = data['action']
+        wordpk = data['target']
 
-    return JsonResponse({'checkStatus': status})
+        word = Word.objects.get(pk=wordpk)
+        if action == 'add':
+            user.liked_words.add(word)
+            status = 1
+        elif action == 'remove':
+            user.liked_words.remove(word)
+            status = 0
+        else:
+            return HttpResponseBadRequest()
+
+        return JsonResponse({'checkStatus': status})
+    except:
+        return HttpResponseBadRequest()
