11fc738b8deb8affe59c78b42163bdeb99a5c9db
benker <benkerd22@gmail.com>
2018-10-29 18:37:15 +0800

    修复笔顺动图Bug

--- jplearn/views.py
+++ jplearn/views.py
@@ -1,22 +1,16 @@
-from functools import wraps
-from django.shortcuts import render
-from django.http import *
-from django.contrib.auth.decorators import login_required
-from django.urls import reverse_lazy
-from jplearn.models import Word, Realuser
+'''
+    views logic for jplearn
+'''
+
 import random
 import urllib
 import json
-
-
 import os
-
-'''
-hiragana = ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と', 'な', 'に', 'ぬ', 'ね',
-            'の', 'は', 'ひ', 'ふ', 'へ', 'ほ', 'ま', 'み', 'む', 'め', 'も', 'や', '', 'ゆ', '', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'わ', '', '', '', 'を', 'ん']
-katakana = ['ア', 'イ', 'ウ', 'エ', 'オ', 'カ', 'キ', 'ク', 'ケ', 'コ', 'サ', 'シ', 'ス', 'セ', 'ソ', 'タ', 'チ', 'ツ', 'テ', 'ト', 'ナ', 'ニ', 'ヌ', 'ネ',
-            'ノ', 'ハ', 'ヒ', 'フ', 'ヘ', 'ホ', 'マ', 'ミ', 'ム', 'メ', 'モ', 'ヤ', '', 'ユ', '', 'ヨ', 'ラ', 'リ', 'ル', 'レ', 'ロ', 'ワ', '', '', '', 'ヲ', 'ン']
-roman = ['a', 'i', 'u', 'e', 'o']'''
+from functools import wraps
+from django.shortcuts import render
+from django.http import *
+from django.urls import reverse_lazy
+from jplearn.models import Word
 
 
 def login_(function=None, status=302):
@@ -53,12 +47,14 @@ def audio(request, q):
 def ttf(request):
     return FileResponse(open('jplearn/font/my.ttf', 'rb'))
 
+
 def img(request, q):
     q = urllib.parse.unquote(q)
     fname = 'jplearn/icon/' + q
     if os.path.exists(fname):
         return FileResponse(open(fname, 'rb'))
     raise Http404()
+
 
 def getRandom(request, action):
     user = request.user.realuser
@@ -92,8 +88,8 @@ def getRandom(request, action):
 
         word = random.choice(real_list)
 
-        request.session['last'] = request.session['current'] # save last word
-        request.session['current'] = word.pk # save current word
+        request.session['last'] = request.session['current']  # save last word
+        request.session['current'] = word.pk  # save current word
 
     if status == 1:
         # show the current word
@@ -127,6 +123,7 @@ def getRandom(request, action):
         'id': word.pk,
         'checked': word.realuser_set.filter(pk=user.pk).exists()
     })
+
 
 def getRound(request, action):
     user = request.user.realuser
@@ -168,7 +165,6 @@ def getRound(request, action):
     })
 
 
-
 @login_
 def test(request):
     try:
@@ -192,7 +188,7 @@ def test(request):
                         'status': 302,
                         'location': reverse_lazy('jplearn:test'),
                     })
-                
+
                 if mode == 'round':
                     return getRound(request, action)
 
@@ -204,38 +200,6 @@ def test(request):
         return HttpResponseNotAllowed(['GET', 'POST'])
     except:
         return HttpResponseBadRequest()
-
-
-'''
-    choice = random.randint(0, len(dic) - 1)
-
-    context = {'kanji':dic[choice][-1], 'gana':dic[choice][0], 'chn':chn[choice]}
-    context['tone'] = ''.join(tonestr[int(t)] for t in tone[choice])
-
-    context['urls'] = [] # generate urls for gif showing
-
-    for x in dic[choice][-1]:
-        url = ''
-        gif = ''
-        if x == 'ん':
-            gif = 'h9n' 
-        elif x == 'ン':
-            gif = 'k9n'
-        elif x in hiragana:
-            index = hiragana.index(x)
-            gif = 'h' + str(index // 5) + roman[index % 5]
-        elif x in katakana:
-            index = katakana.index(x)
-            gif = 'k' + str(index // 5) + roman[index % 5]
-        elif ord(x) in range(0x4e00, 0x9fb0):
-            url = 'http://img.kakijun.com/kanjiphoto/gif/'+ format(ord(x), 'x') + '.gif'
-
-        if not gif == '':
-            url = 'https://kakijun.jp/gif-h-k-al/' + gif + '.gif'
-        
-        context['urls'].append({'name':url, 'alt':x})
-
-    return context'''
 
 
 def initTest(request):
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponse
+
+def simple(request):
+	return HttpResponse('<a href="/jplearn">JpLearn</a>')
 
 urlpatterns = [
+	path('', simple),
     path('jplearn/', include('jplearn.urls')),
     path('admin/', admin.site.urls),
 ]
